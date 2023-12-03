d36f7c70d2cc91f31eb673452ef1fc2c8744f1c3
Martin Camitz <martin.camitz@omegapoint.se>
2023-12-03 21:15:43 +0100

    2 down

--- 2/Program.fs
+++ 2/Program.fs
@@ -1,37 +1,68 @@
 ﻿open System
 open System.Text.RegularExpressions
 
-let split (sep:string) (s:string) = s.Split(sep, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+let split (sep: string) (s: string) =
+    s.Split(sep, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
 
-type blocks = {Red:int; Green:int; Blue:int }
-let defaultBlock = {Red=0;Green=0;Blue=0}
-type Game =
-    { ID:int; Reveals:blocks list }
-    
-let parseBlocks (s:string) =
-   let matches = Regex("(\d+) (red|green|blue)").Matches(s)
+type CubeSet = { Red: int; Green: int; Blue: int }
+let defaultCubeSet = { Red = 0; Green = 0; Blue = 0 }
+type Game = { ID: int; Reveals: CubeSet list }
 
-   matches
-   |> Seq.map (fun m -> (Int32.Parse(m.Groups[1].Value), m.Groups[2].Value))
-   |> Seq.fold (fun acc (n, s) ->
-                    match s with
-                    | "red" -> {acc with Red=n}
-                    | "green" -> {acc with Blue=n}
-                    | _ -> {acc with Green=n}
-                ) defaultBlock 
-        
-let parseLine (line:string) =
-    line.Replace("Game ","")
+let isPossibleReveal (gameSet: CubeSet) (revealSet: CubeSet) =
+    gameSet.Red >= revealSet.Red
+    && gameSet.Green >= revealSet.Green
+    && gameSet.Blue >= revealSet.Blue
+
+let isPossibleGame (gameSet: CubeSet) (game: Game) =
+    game.Reveals |> List.forall (isPossibleReveal gameSet)
+
+let minimumGameSet (game: Game) =
+    let maxCubeSet (a: CubeSet) (b: CubeSet) =
+        { Red = max a.Red b.Red
+          Blue = max a.Blue b.Blue
+          Green = max a.Green b.Green }
+
+    game.Reveals |> List.reduce maxCubeSet
+
+
+let parseCubeSet (s: string) =
+    let matches = Regex("(\d+) (red|green|blue)").Matches(s)
+
+    matches
+    |> Seq.map (fun m -> (Int32.Parse(m.Groups[1].Value), m.Groups[2].Value))
+    |> Seq.fold
+        (fun acc (n, s) ->
+            match s with
+            | "red" -> { acc with Red = n }
+            | "blue" -> { acc with Blue = n }
+            | _ -> { acc with Green = n })
+        defaultCubeSet
+
+let parseLine (line: string) =
+    line.Replace("Game ", "")
     |> split ":"
     |> Array.collect (split ";")
     |> Array.toList
-    |> function 
-        | g::b ->
+    |> function
+        | g :: b ->
             { ID = Int32.Parse g
-              Reveals =  b
-                         |> List.map parseBlocks
-            }
+              Reveals = b |> List.map parseCubeSet }
         | _ -> failwith ""
-let lines = IO.File.ReadLines(__SOURCE_DIRECTORY__ + @"\testinput.txt") |> Seq.toList |> List.map parseLine
 
-printfn $"%A{lines}"
+let games =
+    IO.File.ReadLines(__SOURCE_DIRECTORY__ + @"\input.txt")
+    |> Seq.toList
+    |> List.map parseLine
+
+let result =
+    games
+    |> List.where (isPossibleGame { Red = 12; Green = 13; Blue = 14 })
+    |> List.sumBy (fun g -> g.ID)
+
+printfn $"%A{result}"
+
+let result2 =
+    games |> List.map minimumGameSet |> List.map (fun g -> g.Blue * g.Green * g.Red)
+    |> List.sum
+
+printfn $"%A{result2}"
