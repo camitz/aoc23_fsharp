0d74fb0f57987635d4f1f01f7a8ccaa4d348b5be
Martin Camitz <martin.camitz@omegapoint.se>
2023-12-04 21:14:33 +0100

    Solved 4.2

--- 4/Program.fs
+++ 4/Program.fs
@@ -21,25 +21,40 @@ type Card =
           WinningNrs = line.[1] |> Set.ofArray
           YourNrs = line.[2] |> Set.ofArray }
 
+    member this.nWinningNrs =
+        (this.WinningNrs, this.YourNrs) ||> Set.intersect
+        |> Set.count
+
     member this.value =
-        let nWinningNrs =
-            (this.WinningNrs, this.YourNrs) ||> Set.intersect
-            |> Set.count
-            
-        match nWinningNrs with
+        match this.nWinningNrs with
         | 0 -> 0
         | n -> pown 2 (n-1)
-
-let parseCubeSet (s: string) =
-    let matches = Regex("\d+").Matches(s)
-
-    matches |> Seq.map (fun m -> Int32.Parse(m.Value))
 
 let cards =
     IO.File.ReadLines(__SOURCE_DIRECTORY__ + @"\input.txt")
     |> Seq.toList
     |> List.map Card.parse
+
+let result1 =
+    cards
     |> List.map (fun c -> c.value)
     |> List.sum
 
-printfn $"%A{cards}"
+printfn $"%A{result1}"
+
+let nCardCopies = Array.create cards.Length 1 |> Array.toList
+
+let clone (ns:int list) (at:int) (count:int) (nCopies:int) =
+    let add (a:int) (b:int) = a + b
+
+    ns[..at] @
+    (ns[at+1..at+count] |> List.map (add nCopies)) @
+    ns[at+count+1..]
+        
+let result2 =
+    cards
+    |> List.indexed
+    |> List.fold (fun acc (i, card) -> clone acc i card.nWinningNrs acc.[i]) nCardCopies
+    |> List.sum
+    
+printfn $"%A{result2}"
